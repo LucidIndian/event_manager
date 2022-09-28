5ccf35abfc344dc2ab7ec4545ada3c9261e94fac
Tygh Walters <tyghwalters@gmail.com>
2022-09-27 21:47:37 -0700

    Add README and clean formatting for main program

---
+++ README.md
@@ -0,0 +1,15 @@
+# event_manager
+Project: Event Manager - Ruby Course
+
+Project Goal
+
+- Complete 3 assignments to work with data from a CSV file
+- Project URL: https://www.theodinproject.com/lessons/ruby-event-manager
+
+How to Use the Project
+
+- Run code in IRB
+- Run file from the command line:  $ ruby mastermind.rb 
+- Paste code with Replit
+
+By @TyghWalters - Sept 2022
--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,51 +7,47 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
-# Added by Tygh:
 def clean_phone(raw_phone)
-  # Remove character and spaces so I can measure only the digits
   phone_array = raw_phone.split("")
-  # removing chars that are not one the digits 0-9 w/ a regex `/\d/` 
+  # removing chars that are not a digit 0-9 so I can measure only the digits
   phone_num_only = phone_array.delete_if {|char| !/\d/.match(char)} 
   phone_numbers = phone_num_only
-  # If the phone number is less than 10 or gretaer than 11 digits, assume that it is a bad number
+  # If the phone  is less than 10 or greater than 11 digits, assume it's bad 
   if phone_numbers.length < 10 || phone_numbers.length > 11
     phone_numbers = Array.new(10, "0") # Zeros = Bad number
-  # If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits
-  elsif phone_numbers.length == 11 && phone_numbers[0] == "1" # first number is 1
+  # If the phone number is 11 digits and the first number IS 1, 
+    #trim the 1 and use the remaining 10 digits
+  elsif phone_numbers.length == 11 && phone_numbers[0] == "1" 
     # trim 1, use the remaining 10 digits
     phone_numbers.delete_at(0)
-  # If the phone number is 11 digits and the first number is not 1, then it is a bad number
-  elsif phone_numbers.length == 11 && phone_numbers[0] != "1" # && first number is NOT 1
+  # If the phone is 11 digits and the 1st num is NOT 1, assume it's bad 
+  elsif phone_numbers.length == 11 && phone_numbers[0] != "1" 
     phone_numbers = Array.new(10, "0") # Zeros = Bad number
-  else  # If the phone number is 10 digits, assume that it is good
+  else  # If the phone number is 10 digits, assume that it's good
     phone_numbers
   end
-
-  # Re-add characters (dashes ###-###-####) and convert to string so it displays better?
+  # Re-add dashes ###-###-#### and convert to string so it displays better
   phone_numbers.insert(3, "-")
   phone_numbers.insert(7, "-")
   phone_nums_string = phone_numbers.join("")
   phone_nums_string.to_s
-end # end method
+end 
 
-# Added by Tygh:
 def clean_reg_date(raw_date, hours_hash, dow_hash)
-  formatted_time = Time.strptime(raw_date, "%m/%d/%y %R") #=> 2000-10-31 00:00:00 -0500
-  # Tally the hour of the new_formatted_time
+  formatted_time = Time.strptime(raw_date, "%m/%d/%y %R")
+  # Tally the hour of the formatted_time
   hour = formatted_time.hour
   puts "formatted_time hour is #{hour}"
   hour_symbol = ":" + hour.to_s
   hours_hash[hour_symbol] += 1 # talley for hours count
-  # DsOW
+  # Tally the DOW of the formatted_time
   dow = formatted_time.wday
   puts "DOW is #{dow}"
   dow_symbol = ":" + dow.to_s
   dow_hash[dow_symbol] += 1 # talley for dow count
   formatted_time
-end # end method
+end 
 
-# Added by Tygh:
 hours_hash = Hash.new
 for i in 0..23 do
   # h[:bat] = 3 # => 3
@@ -59,7 +55,6 @@ for i in 0..23 do
   hours_hash[symbol] = 0
 end
 
-# Added by Tygh:
 dow_hash = Hash.new
 for i in 0..6 do
   symbol = ":" + i.to_s
@@ -83,9 +78,7 @@ end
 
 def save_thank_you_letter(id,form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
-
   filename = "output/thanks_#{id}.html"
-
   File.open(filename, 'w') do |file|
     file.puts form_letter
   end
@@ -93,9 +86,7 @@ end
 
 def save_time_targeting(time_target_report)
   Dir.mkdir('time_target_report') unless Dir.exist?('time_target_report')
-
   filename = "time_target_report/time_target_report.html"
-
   File.open(filename, 'w') do |file|
     file.puts time_target_report
   end
@@ -127,13 +118,8 @@ end
 
 # Assignment: Time Targeting
 sorted_hours = hours_hash.sort_by{|k,v| v}.reverse
-p sorted_hours
 bossname = "Brittany"
-
-
 # Assignment: DOW
 sorted_days = dow_hash.sort_by{|k,v| v}.reverse
-p sorted_days
-
 time_target_report = erb_template_b.result(binding)
 save_time_targeting(time_target_report)
